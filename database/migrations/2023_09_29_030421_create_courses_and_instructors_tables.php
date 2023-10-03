599734bede2e02f48221bd7d7ab5e31e69142974
Clintmigue <clintmigue993@gmail.com>
2023-10-03 09:05:04 +0800

    added activity #5

--- database/migrations/2023_09_29_030421_create_courses_and_instructors_tables.php
+++ database/migrations/2023_09_29_030421_create_courses_and_instructors_tables.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestapms();
+            $table->timestamps();
         });
     }
 
